e304601bccd94c78c361cf4c19c7fead64fd1d62
Alun Williams <alun.williams@gmail.com>
2017-01-11 21:02:06 +0000

    Added sum of flags and test

--- ConfusionMatrix/calculations.fs
+++ ConfusionMatrix/calculations.fs
@@ -10,4 +10,9 @@ module Simple =
     let ``When 2 is added to 2 expect 4``() = 
         Assert.AreEqual(add 2 2, 2+2)
 
+    let flags = [1;0;0;0;1;1;]
+    let sumFlags = flags |> List.sumBy (fun i -> i)
 
+    [<Test>]
+    let ``Sum of three ones is 3``() = 
+        Assert.AreEqual(sumFlags, 3)
